39774137051d9ff27ff8aacb2625fda33a970e82
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-26 19:09:59 +0500

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/To_Do_List_App_in_ReactJS/",
+  plugins: [react()],
 })
